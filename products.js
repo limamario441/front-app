e11f6014acc8d50d1fc5acd2c47ed526dde2f846
mario <limamario441@gmail.com>
2023-10-21 10:57:12 -0300

    first commit

---
+++ products.js
@@ -0,0 +1,9 @@
+const menuOptions = [
+    { name: 'combo Burg', price: 50, vegan: false, src: './img/combo-burg.jpg' },
+    { name: 'combo Burg2', price: 34, vegan: false, src: './img/combo-burg2.jpg' },
+    { name: 'Filé de peixe', price: 39, vegan: false, src: './img/filé de peixe.jpg' },
+    { name: 'filé mion', price: 50, vegan: false, src: './img/filé-mion.jpg' },
+    { name: 'linguiça da casa', price: 55, vegan: true, src: './img/linguiça-da-casa.jpg' },
+    { name: 'porco ao limão', price: 45, vegan: true, src: './img/porco ao limao.jpg' },
+  ]
+  
